f496635a0a4c4893e40c03fd3a4e9637e2a595ca
amolpradeshi <pardeshiharshal90@gmail.com>
2022-09-10 20:17:42 +0530

    Created portfolio template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@chakra-ui/react";
+import Styles from "./App.module.css";
+import { useContext } from "react";
+import { AppContext } from "./Context/AppContext.jsx";
+import Home from "./Components/Home.jsx";
 
 function App() {
+  const { isDark } = useContext(AppContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className={isDark ? Styles.darkTheme : Styles.lightTheme}>
+      <Home />
+    </Box>
   );
 }
 

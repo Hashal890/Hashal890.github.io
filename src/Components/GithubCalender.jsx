5dae8518804fe7c54b6f70fab3b62fa4c6f067de
amolpradeshi <pardeshiharshal90@gmail.com>
2022-09-19 18:29:42 +0530

    Changed username of github calender

--- src/Components/GithubCalender.jsx
+++ src/Components/GithubCalender.jsx
@@ -5,10 +5,18 @@ import { Code } from "@chakra-ui/react";
 
 export default function GithubCalendar() {
   return (
-    <Row style={{ justifyContent: "center", marginTop: "2rem", paddingLeft:"0.5rem"  }}>
-      <Code colorScheme="green" mb="1rem" fontSize="18px">Github Calender</Code>
+    <Row
+      style={{
+        justifyContent: "center",
+        marginTop: "2rem",
+        paddingLeft: "0.5rem",
+      }}
+    >
+      <Code colorScheme="green" mb="1rem" fontSize="18px">
+        Github Calender
+      </Code>
       <GitHubCalendar
-        username="PRASHANT7277"
+        username="Hashal890"
         blockSize={15}
         blockMargin={5}
         color="rgb(10,120,219)"

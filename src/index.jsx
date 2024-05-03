2017766020c8d7b09f8603ae7be0d4e478ce6077
amolpradeshi <pardeshiharshal90@gmail.com>
2024-05-03 21:19:23 +0530

    fixed routing issue

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 reportWebVitals();
